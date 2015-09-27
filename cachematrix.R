0f2442f007d7ba3a6abfe27ed6b07340be1c1a50
winnie-faraday <yulixinglj@hotmail.com>
2015-09-27 10:47:37 +0800

    Update cachematrix.R
    makeMatrix <- function(x = matrix()) {
            temp_inverse <- NULL
            set <- function(y) {
                    x <<- y
                    temp_inverse <<- NULL
            }
            get <- function() 
            x
            setinverse <- function(inverse) 
            temp_inverse <<- inverse
            getinverse <- function() 
            temp_inverse
            list(set = set,
                 get = get,
                 setinverse = setinverse,
                 getinverse = getinverse)
    }
    cacheInverse <- function(x, ...) {
     
            temp_inverse <- x$getinverse()
            if (!is.null(temp_inverse)) {
                    message("getting cached data")
                    return(temp_inverse)
            }
            matrix <- x$get()
            temp_inverse <- solve(matrix, ...)
            x$setinverse(temp_inverse)
            temp_inverse
    }

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Write a short comment describing this function:
+## this  assignment is pretty simillar to the example 
+## code which, don't need too much adoption, could be 
+## worked out by using the "solve" function.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ temp_inverse <- NULL
+        set <- function(y) {
+                x <<- y
+                temp_inverse <<- NULL
+        }
+        get <- function() 
+        x
+        setinverse <- function(inverse) 
+        temp_inverse <<- inverse
+        getinverse <- function() 
+        temp_inverse
+        list(set = set,
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Write a short comment describing this function:
+## it is the function that compute the inverse
+## of the matrix we input in the above function 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        temp_inverse <- x$getinverse()
+        if (!is.null(temp_inverse)) {
+                message("getting cached data")
+                return(temp_inverse)
+        }
+        matrix <- x$get()
+        temp_inverse <- solve(matrix, ...)
+        x$setinverse(temp_inverse)
+        temp_inverse
 }
