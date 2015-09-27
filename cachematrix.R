bdb3e176f620dacb4f3a67e96ba22772d9a3ddf8
winnie-faraday <yulixinglj@hotmail.com>
2015-09-27 11:21:39 +0800

    Update cachematrix.R
    makeMatrix <- function(x = matrix()) {
            temp_inverse <- NULL
            set <- function(y) {
                    x <<- y
                    temp_inverse <<- NULL
            }
            get <- function() 
            x
            setinverse <- function(inverse) 
            temp_inverse <<- inverse
            getinverse <- function() 
            temp_inverse
            list(set = set,
                 get = get,
                 setinverse = setinverse,
                 getinverse = getinverse)
    }
    cacheInverse <- function(x, ...) {
     
            temp_inverse <- x$getinverse()
            if (!is.null(temp_inverse)) {
                    message("getting cached data")
                    return(temp_inverse)
            }
            matrix <- x$get()
            temp_inverse <- solve(matrix, ...)
            x$setinverse(temp_inverse)
            temp_inverse
    }

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+temp_inverse <- NULL
+        set <- function(y) {
+                x <<- y
+                temp_inverse <<- NULL
+        }
+        get <- function() 
+        x
+        setinverse <- function(inverse) 
+        temp_inverse <<- inverse
+        getinverse <- function() 
+        temp_inverse
+        list(set = set,
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
@@ -12,4 +26,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        temp_inverse <- x$getinverse()
+        if (!is.null(temp_inverse)) {
+                message("getting cached data")
+                return(temp_inverse)
+        }
+        matrix <- x$get()
+        temp_inverse <- solve(matrix, ...)
+        x$setinverse(temp_inverse)
+        temp_inverse
 }
